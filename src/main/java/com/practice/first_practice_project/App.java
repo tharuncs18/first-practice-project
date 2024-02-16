07a0161aef4badb66b280a1775234b431e7b1368
tharuncs18 <tharuna4218@gmail.com>
2024-02-16 06:17:17 +0000

    added intial commit

---
+++ src/main/java/com/practice/first_practice_project/App.java
@@ -0,0 +1,13 @@
+package com.practice.first_practice_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
